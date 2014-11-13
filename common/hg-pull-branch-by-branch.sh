8d47538f7a00db73596a7cf91db2c34cc6bc5c87
Yury Antonov <yantonov@yandex.ru>
2014-11-13 17:07:55 +0500

    one liner

--- common/hg-pull-branch-by-branch.sh
+++ common/hg-pull-branch-by-branch.sh
@@ -4,9 +4,4 @@
 # but it was possible to pull any selected branch
 # this tiny snippet was helpful to preserve all latest changes
 
-branches=`hg branches | cut -d \  -f 1 | xargs echo `
-
-for br in $branches
-do
-    echo "hg pull -b $br"
-done
+hg branches | cut -d \  -f 1 | xargs -I % hg pull -b %
