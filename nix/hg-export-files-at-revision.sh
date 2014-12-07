5d839d953edc500b05030e10fd9e439f023ffbe4
Yury Antonov <yantonov@yandex.ru>
2014-12-07 23:22:44 +0500

    filenames can contain spaces

--- nix/hg-export-files-at-revision.sh
+++ nix/hg-export-files-at-revision.sh
@@ -7,7 +7,7 @@ DESTINATION=$2
 
 if [ -n "$REVISION" ] && [ -n "$DESTINATION" ]; then
     mkdir -p $DESTINATION
-    hg archive --type files --rev $REVISION -I $(hg log -r $REVISION --template {files} | sed 's/ / -I /g') $DESTINATION
+    hg archive --type files --rev $REVISION -I $(hg log -r $REVISION --template '{files}\n' | sed 's/\n / -I /g') $DESTINATION
 else
     echo -e "`basename $0` revision outputdir"
     echo -e "\twhere revision - revision number or hash"
