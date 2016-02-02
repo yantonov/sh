618a7ba2ca995808f1ec9e5d0f927bddd0f3a5f5
Yury Antonov <yantonov@yandex.ru>
2016-02-02 09:58:40 +0500

    install elastic

---
+++ nix/install-elastic.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+ELASTIC_VERSION="2.1.1"
+
+INSTALL_DIR="$HOME/Development/bin"
+TARGET_DIR="elastic-$ELASTIC_VERSION"
+
+DIST_URL="https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/$ELASTIC_VERSION/elasticsearch-$ELASTIC_VERSION.zip"
+
+PACKAGE_FILENAME="elasticsearch-$ELASTIC_VERSION.zip"
+
+if [ -d "$INSTALL_DIR/$TARGET_DIR" ];
+then
+    echo "elastic $ELASTIC_VERSION is already installed"
+else
+    cd $HOME/Downloads
+    # download
+    curl -L -o $PACKAGE_FILENAME $DIST_URL
+    # extract to install dir
+    unzip -d $INSTALL_DIR/$TARGET_DIR $PACKAGE_FILENAME
+    # cleanup downloaded files
+    rm $PACKAGE_FILENAME
+    cd $INSTALL_DIR
+    # remove old sym link
+    if [ -L "elastic" ];
+    then
+        echo "removing old symlink to elastic dist"
+        rm -iv elastic
+    fi
+    # create symlink
+    ln -s $INSTALL_DIR/$TARGET_DIR elastic
+fi
+
+
