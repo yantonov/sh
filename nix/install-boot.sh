0d64aa7208014be8c1b5dc47649a5241ab44ed56
Yury Antonov <yantonov@yandex.ru>
2016-11-19 23:12:25 +0500

    install boot

---
+++ nix/install-boot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+
+cd ~/Downloads
+
+INSTALL_DIR=~/Development/bin
+BOOT_DIST_URL="https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh"
+BOOT_EXECUTABLE="boot.sh"
+TARGET_DIRECTORY="$INSTALL_DIR/boot/bin"
+
+curl -L $BOOT_DIST_URL -o $BOOT_EXECUTABLE
+
+chmod +x $BOOT_EXECUTABLE
+
+mkdir -p $TARGET_DIRECTORY
+
+mv $BOOT_EXECUTABLE $TARGET_DIRECTORY
+
+echo "boot has successfully installed"
