ee4f36f70dd1d3c41f753ce0256813fe93417546
Yury Antonov <yantonov@yandex.ru>
2017-02-07 13:25:42 +0500

    install hugo on linux

--- mac/install-hugo.sh
+++ mac/install-hugo.sh
@@ -10,13 +10,40 @@ then
     exit 1
 fi
 
+if [ $# -eq 0 ]; then
+    scr=`basename $0`
+    echo "Usage:"
+    echo "${scr} mac|linux - install to given platform"
+    echo "${scr} - show this help"
+    exit 0
+fi
+
 cd ~/Downloads
 
-DIST_URL="https://github.com/spf13/hugo/releases/download/v${VERSION}/hugo_${VERSION}_macOS-64bit.zip"
-TARGET_DIST_FILENAME="hugo-${VERSION}.zip"
-curl -L -o $TARGET_DIST_FILENAME $DIST_URL
+PLATFORM=$1
 
-unzip -d . $TARGET_DIST_FILENAME
+case "$PLATFORM" in
+    "mac")
+        DIST_URL="https://github.com/spf13/hugo/releases/download/v${VERSION}/hugo_${VERSION}_macOS-64bit.zip"
+        TARGET_DIST_FILENAME="hugo-${VERSION}.zip"
+        curl -L -o $TARGET_DIST_FILENAME $DIST_URL
+
+        unzip -d . $TARGET_DIST_FILENAME
+
+        ;;
+    "linux")
+        DIST_URL="https://github.com/spf13/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz"
+        TARGET_DIST_FILENAME="hugo-${VERSION}.tar.gz"
+        curl -L -o $TARGET_DIST_FILENAME $DIST_URL
+
+        tar xfv $TARGET_DIST_FILENAME
+        ;;
+    *)
+        echo "invalid platform"
+        exit 1
+        ;;
+esac
+
 extracted_dir=`find . -type d -name "hugo*" | head -n 1`
 if [ -z "${extracted_dir}" ]; then
     echo "cant find hugo dist"
