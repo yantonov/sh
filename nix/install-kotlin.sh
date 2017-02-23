5a4148029eef03b75d288f365c079f67c35d79aa
Yury Antonov <yantonov@yandex.ru>
2017-02-23 09:57:04 +0500

    check if already installed

--- nix/install-kotlin.sh
+++ nix/install-kotlin.sh
@@ -5,16 +5,22 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 cd ~/Downloads
 
 echo "detect latest kotlin version..."
-KOTLIN_VERSION=`curl -s -L https://github.com/JetBrains/kotlin/releases/latest | grep -E 'kotlin-compiler-([.0-9]+).zip' | sed -E 's/.*\/kotlin-compiler-([.0-9]+)\.zip.*/\1/' | head -n 1`
+VERSION=`curl -s -L https://github.com/JetBrains/kotlin/releases/latest | grep -E 'kotlin-compiler-([.0-9]+).zip' | sed -E 's/.*\/kotlin-compiler-([.0-9]+)\.zip.*/\1/' | head -n 1`
 echo ""
-echo "kotlin $KOTLIN_VERSION will be installed now..."
+echo "kotlin $VERSION will be installed now..."
 echo ""
-KOTLIN_DIST_FILE_NAME="kotlin-compiler-${KOTLIN_VERSION}.zip"
-KOTLIN_DIST_URL="https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VERSION}/kotlin-compiler-${KOTLIN_VERSION}.zip"
+KOTLIN_DIST_FILE_NAME="kotlin-compiler-${VERSION}.zip"
+KOTLIN_DIST_URL="https://github.com/JetBrains/kotlin/releases/download/v${VERSION}/kotlin-compiler-${VERSION}.zip"
 KOTLIN_EXTRACTED_DIR="kotlinc"
-KOTLIN_DESTINATION_DIR="kotlinc-${KOTLIN_VERSION}"
+KOTLIN_DESTINATION_DIR="kotlinc-${VERSION}"
 INSTALL_DIR=~/Development/bin
 KOTLIN_SYMLINK_NAME="kotlinc"
+
+if [ -d "$INSTALL_DIR/$KOTLIN_DESTINATION_DIR" ];
+then
+    echo "kotlin $VERSION is already installed"
+    exit 0
+fi
 
 curl -L $KOTLIN_DIST_URL -o $KOTLIN_DIST_FILE_NAME
 
@@ -33,4 +39,4 @@ rm -rvi kotlinc
 echo "create new symlink"
 ln -s `pwd`/$KOTLIN_DESTINATION_DIR $KOTLIN_SYMLINK_NAME
 
-echo "kotlin ${KOTLIN_VERSION} has successfully installed"
+echo "kotlin ${VERSION} has successfully installed"
