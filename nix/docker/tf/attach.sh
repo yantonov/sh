0a940ca21af2174e600c841afd0b90cf9a15b745
Yury Antonov <yantonov@yandex.ru>
2018-06-16 11:18:54 +0500

    get rid of unused scripts

--- nix/docker/tf/attach.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-image_id=`docker ps | grep 'tensorflow/tensorflow' | awk '{print $1}'`
-
-sudo docker exec -i -t $image_id /bin/bash
--- nix/docker/tf/tf-latest-py3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -p 8888:8888 -p 6006:6006 tensorflow/tensorflow:latest-py3
