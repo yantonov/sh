b8be935bc43712122d3426c74392b6caf3a7e580
Yury Antonov <yantonov@yandex.ru>
2020-01-15 21:40:06 +0500

    use symlink from home/bin for boot executable

--- nix/install-boot.sh
+++ nix/install-boot.sh
@@ -4,17 +4,19 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 cd "${HOME}/Downloads"
 
-INSTALL_DIR="${HOME}/Development/bin"
+DEVELOPMENT_DIR="${HOME}/Development/bin"
 BOOT_DIST_URL="https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh"
 BOOT_EXECUTABLE="boot"
-TARGET_DIRECTORY="${INSTALL_DIR}/boot/bin"
+TARGET_DIRECTORY="${DEVELOPMENT_DIR}/boot/bin"
+BIN_DIR="${HOME}/bin"
 
 curl -L "${BOOT_DIST_URL}" -o "${BOOT_EXECUTABLE}"
-
 chmod +x "${BOOT_EXECUTABLE}"
-
 mkdir -p "${TARGET_DIRECTORY}"
-
 mv "${BOOT_EXECUTABLE}" "${TARGET_DIRECTORY}"
+
+mkdir -p "${BIN_DIR}"
+cd "${BIN_DIR}"
+ln -s "${TARGET_DIRECTORY}" boot
 
 echo "boot has successfully installed"
