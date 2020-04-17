42c0493df2097aba0bbe048053018d1696a80383
Yury Antonov <yantonov@yandex.ru>
2020-04-17 07:20:09 +0500

    disable interface conditionally

--- ubuntu/wifi-up.sh
+++ ubuntu/wifi-up.sh
@@ -2,6 +2,20 @@
 
 if [ -z "$1" ]; then
     echo "Usage: wifi-up.sh SID"
+    exit 1
 else
-  nmcli c up id $1
+    if [ "$1" == "--down" ]; then
+        if [ -z "$2" ]; then
+            echo "Usage: wifi-up.sh --down SID"
+            exit 1
+        else
+            SID="$2"
+            COMMAND="down"
+        fi
+    else
+        SID="$1"
+        COMMAND="up"
+    fi
 fi
+
+nmcli c "${COMMAND}" id "${SID}"
