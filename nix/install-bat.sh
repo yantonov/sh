8de18a6f69569e473554aba545d74c1d70d3aaf7
Yury Antonov <yantonov@yandex.ru>
2020-06-10 12:28:22 +0500

    reduce hardcoded names

--- nix/install-bat.sh
+++ nix/install-bat.sh
@@ -2,22 +2,24 @@
 set -eu
 
 BIN_DIR="${HOME}/bin"
-SOURCE_DIR="${HOME}/Development/git"
+SOURCE_DIR="${HOME}/Development/bin"
 
 mkdir -p "${SOURCE_DIR}"
-cd "${SOURCE_DIR}"
 
-if [ -d "bat" ]; then
-    echo "bat directory is already exist"
+APP_NAME=bat
+
+if [ -d "${APP_NAME}" ]; then
+    echo "${APP_NAME} directory is already exist"
     exit 1;
 fi
 
-git clone https://github.com/sharkdp/bat.git
+cd "${SOURCE_DIR}"
+git clone https://github.com/sharkdp/bat.git ${APP_NAME}
 
-cd bat
+cd ${APP_NAME}
 # for ubuntu
 # apt-get install libclang-dev
 cargo build --release
 
 mkdir -p "${BIN_DIR}"
-ln -s "${SOURCE_DIR}/bat/target/release/bat" "${BIN_DIR}/bat"
+ln -s "${SOURCE_DIR}/bat/target/release/bat" "${BIN_DIR}/${APP_NAME}"
