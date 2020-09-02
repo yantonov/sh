faf5790b89f2c6038b05fcb52fe69ace006a10ab
Yury Antonov <yantonov@yandex.ru>
2020-09-02 20:59:09 +0500

    move rust installation mechanics to makefile

--- nix/install-bat.sh
+++ nix/install-bat.sh
@@ -1,26 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-BIN_DIR="${HOME}/bin"
-SOURCE_DIR="${HOME}/Development/bin"
-URL="https://github.com/sharkdp/bat.git"
+export URL="https://github.com/sharkdp/bat.git"
+export APP_NAME=bat
 
-mkdir -p "${SOURCE_DIR}"
+cd "$(dirname "$0")/rust"
 
-APP_NAME=bat
-
-if [ -d "${APP_NAME}" ]; then
-    echo "${APP_NAME} directory is already exist"
-    exit 1;
-fi
-
-cd "${SOURCE_DIR}"
-git clone "${URL}" "${APP_NAME}"
-
-cd ${APP_NAME}
-# for ubuntu
-# apt-get install libclang-dev
-cargo build --release
-
-mkdir -p "${BIN_DIR}"
-ln -s "${SOURCE_DIR}/${APP_NAME}/target/release/${APP_NAME}" "${BIN_DIR}/${APP_NAME}"
+make build
+make install
