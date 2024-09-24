4aec9519e7f2a6fe87adc4804f4d1ecc598bcf46
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2024-09-24 13:50:57 +0200

    install delta from prebuilt binaries

--- nix/install-delta.sh
+++ nix/install-delta.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 set -eu
 
-export URL="https://github.com/dandavison/delta"
-export APP_NAME=delta
+PLATFORM="$(uname)"
 
-cd "$(dirname "$0")/rust"
+case "${PLATFORM}" in
+    Linux)
+        OS="linux"
+        FILENAME="delta-0.18.2-x86_64-unknown-linux-musl.tar.gz"
+        URL="https://github.com/dandavison/delta/releases/download/0.18.2/${FILENAME}"               
+        ;;
+    Darwin)
+        OS="darwin"
+        echo "not supported"
+        exit 1
+        ;;
+    *)
+        echo "cant detect os automatically"      
+        exit 1
+        ;;
+esac
 
-make build
-make install
+cd "${HOME}/bin"
+curl -L $URL -o $FILENAME
+tar xvfz $FILENAME
+rm $FILENAME
+
+echo "ok"
