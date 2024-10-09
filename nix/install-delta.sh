fc6df189af67becb64c423fb18ae035b06a50bc3
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2024-10-09 17:26:07 +0200

    install delta from prebuilt binaries

--- nix/install-delta.sh
+++ nix/install-delta.sh
@@ -5,14 +5,18 @@ PLATFORM="$(uname)"
 
 case "${PLATFORM}" in
     Linux)
-        OS="linux"
         FILENAME="delta-0.18.2-x86_64-unknown-linux-musl.tar.gz"
-        URL="https://github.com/dandavison/delta/releases/download/0.18.2/${FILENAME}"               
+        URL="https://github.com/dandavison/delta/releases/download/0.18.2/${FILENAME}"
+        UNPACK_COMMAND="tar xvfz"
         ;;
     Darwin)
-        OS="darwin"
         echo "not supported"
         exit 1
+        ;;
+    MINGW*)
+        FILENAME="delta-0.18.2-x86_64-pc-windows-msvc.zip"
+        URL="https://github.com/dandavison/delta/releases/download/0.18.2/${FILENAME}"
+        UNPACK_COMMAND="unzip -d ."        
         ;;
     *)
         echo "cant detect os automatically"      
@@ -22,7 +26,7 @@ esac
 
 cd "${HOME}/bin"
 curl -L $URL -o $FILENAME
-tar xvfz $FILENAME
-rm $FILENAME
+"${UNPACK_COMMAND} $FILENAME"
+rm "${FILENAME}"
 
 echo "ok"
