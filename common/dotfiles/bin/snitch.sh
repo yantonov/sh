ea8f136cacbf034a39df23b5b2bd77c173bfee91
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-07-23 09:39:12 +0200

    add snitch repo url

--- common/dotfiles/bin/snitch.sh
+++ common/dotfiles/bin/snitch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 set -o errexit -o nounset
 
+# https://github.com/karol-broda/snitch
 docker run -it --rm --net=host --pid=host --cap-add=SYS_PTRACE ghcr.io/karol-broda/snitch:latest-ubuntu
